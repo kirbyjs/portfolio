298210d09d5630aabaea521e2213acbe5424427c
Kirby Simmons <kirbyjsimmons@gmail.com>
2019-12-15 21:50:50 -0600

    ci: add bucket policy to allow everyone read access

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "portfolio" {
   bucket = "kirbyjs-portfolio"
   acl    = "private"
+  policy = file("${path.cwd}/resources/bucket-policy.json")
 
   tags = {
     Name = "kirbyjs-portfolio"
