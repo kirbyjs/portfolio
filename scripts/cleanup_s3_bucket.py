e189275a3932bb78e8f4753ae4eaf46fc42eebd0
Kirby Simmons <kirbyjsimmons@gmail.com>
2020-03-20 22:00:37 -0500

    feat: delete old objects from s3

--- scripts/cleanup_s3_bucket.py
+++ scripts/cleanup_s3_bucket.py
@@ -4,12 +4,10 @@ import datetime
 
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket('kirbyjs.com')
-
 latest_last_modified_date = my_bucket.Object('index.html').last_modified
 
 for obj in my_bucket.objects.all():
     last_modified = obj.Object().last_modified
 
     if (last_modified < latest_last_modified_date - datetime.timedelta(0, 30)):
-        print('object will be deleted:')
-        print(obj)
+        obj.Object().delete()
