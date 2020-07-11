f4bdb1d70303a0bb798380712aab606428109c69
Kirby Simmons <kirbyjsimmons@gmail.com>
2020-07-10 23:04:34 -0500

    feat: programmatically set all index.html cache-control headers to no-store

---
+++ scripts/cache-control-s3-files.py
@@ -0,0 +1,12 @@
+
+import boto3
+import datetime
+
+s3 = boto3.resource('s3')
+my_bucket = s3.Bucket('kirbyjs.com')
+
+for obj in my_bucket.objects.all():
+    Object = obj.Object()
+
+    if (Object.key.endswith('index.html')):
+        Object.put(CacheControl='no-store')
--- scripts/cleanup_s3_bucket.py
+++ scripts/cleanup_s3_bucket.py
@@ -7,7 +7,7 @@ my_bucket = s3.Bucket('kirbyjs.com')
 latest_last_modified_date = my_bucket.Object('index.html').last_modified
 
 for obj in my_bucket.objects.all():
-    last_modified = obj.Object().last_modified
+    Object = obj.Object()
 
-    if (last_modified < latest_last_modified_date - datetime.timedelta(0, 30)):
-        obj.Object().delete()
+    if (Object.last_modified < latest_last_modified_date - datetime.timedelta(0, 30)):
+        Object.delete()
